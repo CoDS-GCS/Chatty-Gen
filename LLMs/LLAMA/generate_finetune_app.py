435224671fe6c6f2db092f693306849dde5b2841
ozx1812 <om.mkp1812@yahoo.com>
2023-08-19 18:28:10 +0000

    add | alpaca-lora LLAMA + LORA-PEFT
    added original and custom llama finetuning scripts from alpaca-lora repo

---
+++ LLMs/LLAMA/generate_finetune_app.py
@@ -0,0 +1,72 @@
+# generate_finetune_app.py
+# Author: @ozx1812
+# Date: 2023/08/18
+
+# The following code has been adopted and modified from [alpaca-loara].
+# Source: https://github.com/tloen/alpaca-lora
+
+# Modifications:
+# Flask endpoint for LLAMA + PEFT text generation
+
+
+from flask import Flask, request, jsonify
+from llama_singleton import ModelSingleton
+from queue import Queue
+from threading import Thread
+
+app = Flask(__name__)
+model_instance = ModelSingleton()
+
+
+class InferenceWorker(Thread):
+    def __init__(self, queue):
+        super().__init__()
+        self.queue = queue
+
+    def run(self):
+        while True:
+            instruction, response_queue = self.queue.get()
+            try:
+                # Perform inference using the preloaded model instance
+                # ...
+                generated_text = model_instance.generate_response(instruction)
+
+                # Add the generated response to the response queue
+                response_queue.put(generated_text)
+            except Exception as e:
+                error_response = {"error": str(e)}
+                response_queue.put(error_response)
+            self.queue.task_done()
+
+
+inference_queue = Queue()
+for _ in range(2):  # Number of worker threads
+    worker = InferenceWorker(inference_queue)
+    worker.daemon = True
+    worker.start()
+
+
+@app.route('/generate', methods=['POST'])
+def generate_text():
+    try:
+        data = request.json
+        instruction = data.get("instruction")
+        # Add your parameters here
+        # ...
+        response_queue = Queue()
+
+        # Add the request to the inference queue
+        inference_queue.put((instruction, response_queue))
+
+        # Wait for the response from the worker thread
+        generated_response = response_queue.get()
+        response_queue.task_done()
+
+        return jsonify({"response": generated_response})
+    except Exception as e:
+        error_response = {"error": str(e)}
+        return jsonify(error_response), 500
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
---
+++ LLMs/LLAMA/llama_singleton.py
@@ -0,0 +1,127 @@
+# llama_singleton.py
+# Author: @ozx1812
+# Date: 2023/08/18
+
+# The following code has been adopted and modified from [alpaca-loara].
+# Source: https://github.com/tloen/alpaca-lora
+
+# Modifications:
+# Singleton LLAMA + PEFT Adapter
+
+import os
+import sys
+import torch
+from peft import PeftModel
+from transformers import GenerationConfig, LlamaForCausalLM, LlamaTokenizer
+
+if torch.cuda.is_available():
+    device = "cuda"
+else:
+    device = "cpu"
+
+try:
+    if torch.backends.mps.is_available():
+        device = "mps"
+except:  # noqa: E722
+    pass
+
+
+class ModelSingleton:
+    _instance = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            load_8bit = False,
+            base_model = "decapoda-research/llama-7b-hf"
+            lora_weights = "0mij/llama-dblp-adapter-v1"
+            base_model = base_model or os.environ.get("BASE_MODEL", "")
+            assert (
+                base_model
+            ), "Please specify a --base_model, e.g. --base_model='huggyllama/llama-7b'"
+
+            cls._instance = super().__new__(cls)
+            cls._instance.tokenizer = LlamaTokenizer.from_pretrained(base_model)
+
+            if device == "cuda":
+                model = LlamaForCausalLM.from_pretrained(
+                    base_model,
+                    load_in_8bit=load_8bit,
+                    torch_dtype=torch.float16,
+                    device_map="auto",
+                )
+                cls._instance.model = PeftModel.from_pretrained(
+                    model,
+                    lora_weights,
+                    torch_dtype=torch.float16,
+                )
+            elif device == "mps":
+                model = LlamaForCausalLM.from_pretrained(
+                    base_model,
+                    device_map={"": device},
+                    torch_dtype=torch.float16,
+                )
+                cls._instance.model = PeftModel.from_pretrained(
+                    model,
+                    lora_weights,
+                    device_map={"": device},
+                    torch_dtype=torch.float16,
+                )
+            else:
+                model = LlamaForCausalLM.from_pretrained(
+                    base_model, device_map={"": device}, low_cpu_mem_usage=True
+                )
+                cls._instance.model = PeftModel.from_pretrained(
+                    model,
+                    lora_weights,
+                    device_map={"": device},
+                )
+
+            # unwind broken decapoda-research config
+            cls._instance.model.config.pad_token_id = cls._instance.tokenizer.pad_token_id = 0  # unk
+            cls._instance.model.config.bos_token_id = 1
+            cls._instance.model.config.eos_token_id = 2
+
+            if not load_8bit:
+                cls._instance.model.half()  # seems to fix bugs for some users.
+
+            cls._instance.model.eval()
+            if torch.__version__ >= "2" and sys.platform != "win32":
+                cls._instance.model = torch.compile(cls._instance.model)
+
+        return cls._instance
+
+    def generate_response(
+        self,
+        instruction,
+        temperature=0.1,
+        top_p=0.75,
+        top_k=40,
+        num_beams=4,
+        max_new_tokens=128,
+        **kwargs,
+    ):
+        prompt = instruction
+        inputs = self.tokenizer(prompt, return_tensors="pt")
+        input_ids = inputs["input_ids"].to(self.model.device)
+        generation_config = GenerationConfig(
+            temperature=temperature,
+            top_p=top_p,
+            top_k=top_k,
+            num_beams=num_beams,
+            **kwargs,
+        )
+
+        # Without streaming
+        with torch.no_grad():
+            generation_output = self.model.generate(
+                input_ids=input_ids,
+                generation_config=generation_config,
+                return_dict_in_generate=True,
+                output_scores=True,
+                max_new_tokens=max_new_tokens,
+            )
+        s = generation_output.sequences[0]
+        output = self.tokenizer.decode(s)
+        return output.strip()
+# Usage:
+# model_instance = ModelSingleton()
